99495c7a10cab50f5fbc8b1d9aaf75c39e344086
Dominic Sheldon <dsheldon@sgi.sk.ca>
2022-01-14 15:48:14 -0600

    Handling click events

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -5,7 +5,7 @@ import './Tile.css'
 const Tile = (props) => {
   const colour = props.selected || props.matched ? {backgroundColor: props.color} : null;
   return (
-    <div className='Tile' style={colour}>
+    <div className='Tile' style={colour} onClick={() => props.handleTileClicked(props.id, props.color)}>
       {props.selected || props.matched ? <props.svg />  : null}
     </div>
   )
