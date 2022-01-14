3269b8b7e53b24f6ace479a1df501b500d4e0ef7
Dominic Sheldon <dsheldon@sgi.sk.ca>
2022-01-14 16:16:50 -0600

    Creating a custom Hook

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -54,13 +54,17 @@ class App extends Component{
     })
   }
 
+  handleNumTileChange = (num) => {
+    this.setState({numTiles: num, playing: false, tiles: []});
+  }
+
   render() {
   return (
     <div className="App">
       <header className="App-header">
         Turbo-Matcher
       </header>
-        <OptionsPanel playing={this.state.playing} numTiles={this.state.numTiles} startGame={this.startGame} />
+        <OptionsPanel playing={this.state.playing} numTiles={this.state.numTiles} startGame={this.startGame} handleNumTileChange={this.handleNumTileChange} />
         <Board numTiles={this.state.numTiles} tiles={this.state.tiles} />
     </div>
   );
